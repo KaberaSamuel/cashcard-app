8ec858ce03a7addccc8a9b6da15ee7325c17cadc
KaberaSamuel <kaberanshutis@gmail.com>
2025-12-28 08:50:14 +0200

    add json serialization/deserialization unit tests

--- src/main/java/example/cashcard/CashcardApplication.java
+++ src/main/java/example/cashcard/CashcardApplication.java
@@ -1,4 +1,4 @@
-package example.cashcard.cashcard;
+package example.cashcard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
